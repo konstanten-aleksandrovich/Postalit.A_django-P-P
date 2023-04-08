a80dc698f31c845fb6d6ffce63f6830885e13ade
Konstantin <ckammmmm@gmail.com>
2023-04-08 15:20:31 +0500

    4 глава

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
+    path('products/',products,name='products'),
+    path('products/<int:ptoductid>/',products,name='productsid'),
+    path('users/',users,name='users'),
+    path('users/<int:id>/<name>/',users,name='user'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hellow world')
+    return HttpResponse('<h2>Hellow world</h2>')
+def about(request):
+    return HttpResponse('<h2>О саите</h2>')
+def contact(request):
+    return HttpResponse('<h2>Контакт</h2>')
 
+def products(request,ptoductid=1):
+    cat=request.GET.get('cat','NO')
+    return  HttpResponse(f'<h2>Продукт № {ptoductid} Категория {cat}</h2>')
+def users(request,id=0,name=''):
+    id=request.GET.get('id','0')
+    name=request.GET.get('name','No name')
+    return  HttpResponse(f'<h2>Пользователь</h2><h3>id: {id}\nИмя: {name}</hЗ>')
