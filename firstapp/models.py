afa1837440b270981648110f3bd358fa2c9d9f77
Konstantin <ckammmmm@gmail.com>
2023-04-11 10:22:53 +0500

    7 глава

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    user=models.CharField(max_length=20)
+    age=models.ImageField()
+    objects=models.Manager()
+    DoesNotExist = models.Manager
+
+class Company(models.Model):
+    mame=models.CharField(max_length=30)
+
+class Product(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    name=models.CharField(max_length=30)
+    price=models.ImageField()
